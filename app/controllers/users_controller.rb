4c7ea2f49220c15f6e563ff8e1baca64f39e561e
Ai-R0921 <xoa1xo.0921@gmail.com>
2023-05-04 12:52:29 +0000

    [WIP]ユーザー編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to my_page_path
+    end
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :email, :is_deleted)
   end
 end
